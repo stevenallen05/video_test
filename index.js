b3a01528a448e7286258cce9a9e7ef965acfd0cd
Steven Allen <sallen@tractionguest.com>
2019-11-17 15:43:28 -0800

    Basic material css

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ console.log("stuff");
 var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2RiNTk0MzEwZjUyODQyOTI2NDUzYTIzZGNiNjJkODIwLTE1NzQwMzIzOTkiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJndWVzdCIsInZpZGVvIjp7InJvb20iOiI3ODBkODkxYS1hNDBmLTRhNzEtYThiZi04NDcxODQ4N2YyYzgifX0sImlzcyI6IlNLZGI1OTQzMTBmNTI4NDI5MjY0NTNhMjNkY2I2MmQ4MjAiLCJuYmYiOjE1NzQwMzIzOTksImV4cCI6MTU3NDAzNTk5OSwic3ViIjoiQUNhYjNjMTY1ZDBhZjM2NTM0Mjc2ZmM4NDQwNDQwN2YwNCJ9.TE80bY1VITscR3y504cIYjnKoDtCQfXuD8ENHW2NUF4";
 const { connect, createLocalTracks } = require('twilio-video');
 
+M.AutoInit();
+
 createLocalTracks({
   audio: true,
   video: { height: 720 }
@@ -24,9 +26,9 @@ createLocalTracks({
   room.on('disconnected', room => {
     // Detach the local media elements
     room.localParticipant.tracks.forEach(publication => {
-      const attachedElements = publication.track.detach();
+      const attachedElements = publication.detach();
       attachedElements.forEach(element => element.remove());
-      publication.track.disable();
+      publication.disable();
     });
   });
 
