bb7217ed6763463443c3f4db7e3345341bc44246
Steven Allen <sallen@tractionguest.com>
2019-11-19 11:58:46 -0800

    Westworld is awesome. When is the next season?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 console.log("stuff");
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2RiNTk0MzEwZjUyODQyOTI2NDUzYTIzZGNiNjJkODIwLTE1NzQwMzk4ODEiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJndWVzdCIsInZpZGVvIjp7InJvb20iOiJiZTEyMzg3Yy02NTJhLTQ4ZDUtYmZhNi01NTZkZGNiMDkzMWQifX0sImlzcyI6IlNLZGI1OTQzMTBmNTI4NDI5MjY0NTNhMjNkY2I2MmQ4MjAiLCJuYmYiOjE1NzQwMzk4ODEsImV4cCI6MTU3NDA0MzQ4MSwic3ViIjoiQUNhYjNjMTY1ZDBhZjM2NTM0Mjc2ZmM4NDQwNDQwN2YwNCJ9.WmmHegR85CFhKpTbxCiGkQEDCpK9zV54vH0GKrNwxDs";
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2RiNTk0MzEwZjUyODQyOTI2NDUzYTIzZGNiNjJkODIwLTE1NzQwNDM2MTYiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJndWVzdCIsInZpZGVvIjp7InJvb20iOiJlNjJkZWIzMC05OWNiLTQ0YzMtODlkMi05M2I4ZDgyZDc5YjkifX0sImlzcyI6IlNLZGI1OTQzMTBmNTI4NDI5MjY0NTNhMjNkY2I2MmQ4MjAiLCJuYmYiOjE1NzQwNDM2MTYsImV4cCI6MTU3NDA0NzIxNiwic3ViIjoiQUNhYjNjMTY1ZDBhZjM2NTM0Mjc2ZmM4NDQwNDQwN2YwNCJ9.DS7weE4noOeyXXKGLlt25FStWLm7fgaalwULKUOjT9Q";
 const { connect, createLocalTracks } = require('twilio-video');
 
 M.AutoInit();
@@ -19,8 +19,17 @@ createLocalTracks({
 }).then(room => {
   console.log(`Connected to Room: ${room.name}`);
 
-  room.on('participantConnected', participant => {
-    console.log(`A remote Participant connected: ${participant}`);
+  room.participants.forEach(participant => {
+    participant.tracks.forEach(publication => {
+      if (publication.isSubscribed) {
+        const track = publication.track;
+        document.getElementById('remote-media').appendChild(track.attach());
+      }
+    });
+  
+    participant.on('trackSubscribed', track => {
+      document.getElementById('remote-media').appendChild(track.attach());
+    });
   });
 
   room.on('disconnected', room => {
@@ -30,7 +39,31 @@ createLocalTracks({
       attachedElements.forEach(element => element.remove());
       publication.disable();
     });
+
+    room.participants.forEach(participant => {
+      participant.localTracks.forEach(publication => {
+        const attachedElements = publication.detach();
+        attachedElements.forEach(element => element.remove());
+        publication.disable();
+      })
+    });
   });
+
+  room.on('participantConnected', participant => {
+    console.log(`Participant "${participant.identity}" connected`);
+  
+    participant.tracks.forEach(publication => {
+      if (publication.isSubscribed) {
+        const track = publication.track;
+        document.getElementById('remote-media').appendChild(track.attach());
+      }
+    });
+  
+    participant.on('trackSubscribed', track => {
+      document.getElementById('remote-media').appendChild(track.attach());
+    });
+  });
+  
 
   document.getElementById('btnDisconnect').onclick = function(){
     room.disconnect();
